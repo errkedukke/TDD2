c3d928faca0bee02d012a2a72cf2663908cd5215
Guga  Rukhadze <Guga.Rukhadze@coxautoinc.com>
2025-01-06 15:50:19 +0400

    Solution cleanup. Starover.

--- TDD.BerlinClock/BerlinClock.cs
+++ TDD.BerlinClock/BerlinClock.cs
@@ -2,37 +2,4 @@
 
 public class BerlinClock
 {
-    public string GetSecondsLamp(int seconds)
-    {
-        return seconds % 2 == 0 ? "Y" : "O";
-    }
-
-    public string GetHoursRow1(int hours)
-    {
-        var lampsOn = hours / 5;
-        return GetLampRow("R", 4, lampsOn);
-    }
-
-    public string GetHoursRow2(int hours)
-    {
-        var lampsOn = hours % 5;
-        return GetLampRow("R", 4, lampsOn);
-    }
-
-    public string GetMinutesRow1(int minutes)
-    {
-        var lampsOn = minutes / 5;
-        return GetLampRow("Y", 11, lampsOn).Replace("YYY", "YYR");
-    }
-
-    public string GetMinutesRow2(int minutes)
-    {
-        var lampsOn = minutes % 5;
-        return GetLampRow("Y", 4, lampsOn);
-    }
-
-    private string GetLampRow(string lampColor, int totalLamps, int lampsOn)
-    {
-        return new string(lampColor[0], lampsOn).PadRight(totalLamps, 'O');
-    }
 }
